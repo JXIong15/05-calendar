9c2c82086995e66f965a5fbf2af7d5c7fb0d1520
Pikachu <jouxiong15@gmail.com>
2021-03-04 21:46:04 -0600

    troubleshooting rows

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,12 @@
 $(document).ready(function () {
     $('#currentDay').text(moment().format('dddd, MMMM Do, YYYY'));
-    
+    var timeBlock = $("#time-block");
     var inputEl = $("#task");
+    var currentTime = moment().format('LT');
+
+    // array of business hours
+    var hours = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
+      
     // when save button is clicked, the text is saved to local storage
     $("#saveBtn").on("click", function() {
         console.log(inputEl.val());
@@ -15,22 +20,32 @@ $(document).ready(function () {
     })
 
     function createTimeBlocks() {
-        let timeBlock = $("#row");
-        // array of business hours
-        let hours = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
-        // create time blocks
+    // create time blocks
         hours.forEach(function(i){ 
-            let timeEl = $("<p>").val(i).text(i).attr("type", "p").attr("border", "solid");
+            let rowEl = $("#row");
+            let timeEl = $("<label>").val(i).text(i);
             let input = $("<input>").attr("type", "text").attr("size", "100%");
             var button = $("<button>").text("💾").attr("type", "button");
-            timeBlock.append(timeEl).append(input).append(button);
+            rowEl.append(timeEl).append(input).append(button).append("<br>");
+            timeBlockColor(timeEl, rowEl);
+            console.log(rowEl[0]);
         }) 
-        
+    }
+    
+    function timeBlockColor(timeEl, rowEl) {
+        if (timeEl > currentTime) {
+            rowEl.attr("background-color", "grey");
+        }
+        else if (timeEl < currentTime) {
+            rowEl.attr("backgrounf-color", "green");
+        }
+        else {
+            rowEl.attr("backgrounf-color", "red");
+        }
+    }
         // if block is in the past, code block is gray 
         // if code block is in the present, block is red 
         // if block is in the future, block is green 
-    }
-    
 
     createTimeBlocks();
 })
