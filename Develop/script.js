9249daaab37707cb4a21d14dbd6e01a49055ec47
Pikachu <jouxiong15@gmail.com>
2021-03-05 01:23:01 -0600

    added CSS elements

--- Develop/script.js
+++ Develop/script.js
@@ -3,46 +3,45 @@ $(document).ready(function () {
     var timeBlock = $("#time-block");
     // array of business hours
     var hours = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
-    var inputEl = $("#task");
-
-    // when save button is clicked, the text is saved to local storage
-    $("#saveBtn").click(function () {
-        console.log(inputEl.val());
-        let myInput = inputEl.val();
-
-        let eventEl = localStorage.getItem("event") || "";
-        localStorage.setItem("event", myInput);
-        inputEl.text(myInput);
-    })
 
     function createTimeBlocks() {
         // create time blocks
         hours.forEach(function (i) {
-            var rowEl = $("<div id='row'>");
-            let timeEl = $("<label>").addClass("textarea").val(i).text(i).attr("type", "label");
+            var rowEl = $("<div>").addClass("row");
+            let timeEl = $("<label>").addClass("hour").val(i).text(i).attr("type", "label");
             let input = $("<input id='task'>").attr("type", "text");
-            var button = $("<button id='saveBtn'>").text("💾").attr("type", "button");
+            let button = $("<button>").addClass("saveBtn").text("💾").attr("type", "button");
+
+            var inputEl = $("#task"); // Problem: 8 textboxes called "task", so there will be a saving error
+            // when save button is clicked, the text is saved to local storage
+            button.click(function() {
+                console.log("Hello");
+                console.log(inputEl.val());
+                let myInput = inputEl.val();
+
+                let eventEl = localStorage.getItem("event") || "";
+                localStorage.setItem("event", myInput);
+                inputEl.text(myInput);
+            })
+
             rowEl.append(timeEl).append(input).append(button);
             timeBlock.append(rowEl);
             // timeBlockColor(timeEl, rowEl);
         })
-
     }
 
     // function timeBlockColor(timeEl, rowEl) {
     //     if (timeEl > currentTime) {
-    //         rowEl.attr("background-color", "grey");
+    //         rowEl.addClass("past");
     //     }
     //     else if (timeEl < currentTime) {
-    //         rowEl.attr("backgrounf-color", "green");
+    //         rowEl.addClass("present");
     //     }
     //     else {
-    //         rowEl.attr("backgrounf-color", "red");
+    //         rowEl.addClass("future");
     //     }
     // }
-    // if block is in the past, code block is gray 
-    // if code block is in the present, block is red 
-    // if block is in the future, block is green 
 
+    
     createTimeBlocks();
 })
