3de63ac9a26e8882675341319e45054ecec7ce62
Pikachu <jouxiong15@gmail.com>
2021-03-06 15:08:13 -0600

    fixed color blocks

--- Develop/script.js
+++ Develop/script.js
@@ -2,22 +2,24 @@ $(document).ready(function () {
     $('#currentDay').text(moment().format('dddd, MMMM Do, YYYY'));
     var timeBlock = $("#time-block");
     // array of business hours
-    var hours = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
+    var hours = ["9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM"];
 
     function createTimeBlocks() {
         // create time blocks
         hours.forEach(function (i) {
+
+            var task = localStorage.getItem(i);
+
             var rowEl = $("<div>").addClass("row");
             let timeEl = $("<label>").addClass("hour col-12 col-lg-1").val(i).text(i).attr("type", "label");
-            let input = $("<input id='task'>").addClass("col-12 col-lg-10").attr("type", "text");
+            let input = $("<input id='task'>").addClass("col-12 col-lg-10").attr("type", "text").val(task);
             let button = $("<button>").addClass("saveBtn col-12 col-lg-1").text("💾").attr("type", "button");
 
-            button.click(function(i) {
-                console.log(input.val());
+            // when the button is clicked, then the input text is saved to the local storage
+            button.click(function (i) {
                 let myInput = input.val();
-
-                let eventEl = localStorage.getItem("event") || "";
-                localStorage.setItem("event", myInput);
+                let hour = timeEl.val();
+                localStorage.setItem(hour, myInput);
                 input.text(myInput);
             })
 
@@ -27,12 +29,20 @@ $(document).ready(function () {
         })
     }
 
+    // color codes the timeblocks. A time in the past is grey, the present is red, and the future is green.
     function timeBlockColor(timeEl, input) {
-        let currentTime = moment().format('LT');
-        if (timeEl > currentTime) {
+        let currentTime = moment().hours();
+        let ourTime = parseInt(timeEl.val().split(" ")[0]);
+        
+        // currentTime is in military time. To make ourTime in military time, add 12 to the values of ourTime less than 6.
+        if (ourTime < 6) {
+            ourTime = ourTime + 12;
+        }
+        
+        if (ourTime < currentTime) {
             input.addClass("past");
         }
-        else if (timeEl < currentTime) {
+        else if (ourTime === currentTime) {
             input.addClass("present");
         }
         else {
@@ -40,6 +50,5 @@ $(document).ready(function () {
         }
     }
 
-    
     createTimeBlocks();
 })
